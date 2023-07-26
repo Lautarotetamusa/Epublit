661c716f4727138a0748a44fe6a60e0be9d3e898
Lautaro Teta Musa <51541903+Lautarotetamusa@users.noreply.github.com>
2023-07-26 01:17:28 -0300

    dev env (#9)

---
+++ api/compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#rm -rf dist/src
+#rm -rf dist/tests
+tsc --project .
+
+cp -r ./src/afip/Claves ./dist/src/afip/Claves
+cp -r ./src/afip/ClavesLibrosSilvestres ./dist/src/afip/ClavesLibrosSilvestres
+cp -r ./src/afip/afip.js/src/Afip_res ./dist/src/afip/afip.js/src/Afip_res
+
+cp -r ./src/comprobantes/factura ./dist/src/comprobantes/factura
+cp -r ./src/comprobantes/remito ./dist/src/comprobantes/remito
