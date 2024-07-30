a21436a13761052c989ff41c8b911d762686389c
lautarotetamusa <lautarotetamusa@gmail.com>
2024-07-30 20:43:31 -0300

    changes

--- api/test/cliente.test.ts
+++ api/test/cliente.test.ts
@@ -88,17 +88,23 @@ test('login', async () => {
         username: 'teti',
         password: 'Lautaro123.'
     }
-    const res = await request(app)
-        .post('/user/login')
-        .send(data)
 
-    expect_success_code(200, res);
-    expect(res.body).toHaveProperty('token');
-    expect(res.body.token).not.toBeUndefined();
-    expect(res.body.token).not.toBeNull();
-    expect(res.body.token).not.toBeFalsy();
-    token = res.body.token;
-    console.log("token: ", token);
+    try{
+        const res = await request(app)
+            .post('/user/login')
+            .send(data)
+
+        expect_success_code(200, res);
+        expect(res.body).toHaveProperty('token');
+        expect(res.body.token).not.toBeUndefined();
+        expect(res.body.token).not.toBeNull();
+        expect(res.body.token).not.toBeFalsy();
+        token = res.body.token;
+        console.log("token: ", token);
+    } catch (e){
+        console.log("ERROR");
+        console.log(e);
+    }
 });
 
 describe('POST cliente/', () => {
