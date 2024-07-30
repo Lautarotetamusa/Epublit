3cbe538267693864b87e0770275abec641c463b4
lautarotetamusa <lautarotetamusa@gmail.com>
2024-07-30 18:06:33 -0300

    debuggin

--- dbtest/test.sql
+++ dbtest/test.sql
@@ -20,6 +20,8 @@
 --
 --CREATE USER 'test_user'@'%' IDENTIFIED BY 'test_pass';
 --GRANT ALL PRIVILEGES ON *.* TO 'test_user'@'%' WITH GRANT OPTION;
+CREATE USER 'teti'@'%' IDENTIFIED BY 'Lautaro123.';
+GRANT ALL PRIVILEGES ON *.* TO 'teti'@'%' WITH GRANT OPTION;
 
 DROP DATABASE IF EXISTS epublit_test;
 CREATE DATABASE IF NOT EXISTS epublit_test;
